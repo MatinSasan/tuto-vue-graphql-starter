4c0af44ebf8a48dfaf46574d354006e2a87fdc4c
Matin Sasanpour <matin.sasan@gmail.com>
2020-01-30 20:43:03 +0330

    added mutation | added signupUser function

---
+++ resolvers.js
@@ -0,0 +1,14 @@
+module.exports = {
+  Mutation: {
+    signupUser: async (_, { username, email, password }, { User }) => {
+      const user = await User.findOne({ username });
+      if (user) throw Error('User already exists');
+      const newUser = await new User({
+        username,
+        email,
+        password
+      }).save();
+      return newUser;
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require('./resolvers');
+
 require('dotenv').config({ path: './variables.env' });
 
 const User = require('./models/User');
@@ -18,19 +25,9 @@ const Post = require('./models/Post');
   }
 })();
 
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-
-  type Query {
-    getTodos: [Todo]
-  }
-`;
-
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   context: {
     User,
     Post
